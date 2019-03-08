be0a07ecc3f5c09f49b3ffaa5f029cc7932b4f3e
Yogita Verma <veryogita@gmail.com>
2019-03-08 22:50:09 +0530

    string truncated and no image logic added

--- src/Container.js
+++ src/Container.js
@@ -1,21 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import Movie from "./Movie";
 import NotFound from "./NotFound";
 
-function Container(props) {
+const Container = props => {
   let return_movie_item;
+
   console.log("container prop", props.movies);
-  props.movies === undefined
+  console.log("container prop length", props.movies.length);
+  props.movies.length === 0
     ? (return_movie_item = <NotFound />)
     : (return_movie_item = props.movies.map(movie => (
         <Movie movie_item={movie} key={movie.id} />
       )));
-
   return (
     <div className="container">
       <div className="row">{return_movie_item}</div>
     </div>
   );
-}
+};
 
 export default Container;
--- src/Movie.js
+++ src/Movie.js
@@ -1,21 +1,26 @@
 import React from "react";
+import noimage from "./no-image.jpg";
 
 function Movie(props) {
   const cardStyle = {
     width: "18rem"
   };
+  let img;
+  if (props.movie_item.poster_path == null) img = noimage;
+  else img = `https://image.tmdb.org/t/p/w500/${props.movie_item.poster_path}`;
+  console.log("props of movie componet", props.movie_item);
   console.log("Poster of movie componet", props.movie_item.poster_path);
   console.log("Title of movie componet", props.movie_item.title);
   return (
     <div className="col col-lg-4">
       <div className="card text-center mb-4" style={cardStyle}>
-        <img
-          src={`https://image.tmdb.org/t/p/w500/${props.movie_item.poster_path}`}
-          className="card-img-top"
-          alt="movie_image"
-        />
+        <img src={img} className="card-img-top" alt="movie_image" />
         <div className="card-body">
-          <h5 className="card-title text-center">{props.movie_item.title}</h5>
+          <h5 className="card-title text-center">
+            {props.movie_item.title.length < 14
+              ? `${props.movie_item.title}`
+              : `${props.movie_item.title.substring(0, 15)}...`}
+          </h5>
 
           <a href="#" className="btn btn-primary mx-auto">
             Details
