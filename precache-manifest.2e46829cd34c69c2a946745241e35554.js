941017ebe74fb95fff3b1e1c92f26febfcfcd2e5
Yog9 <veryogita@gmail.com>
2019-07-04 15:49:13 +0530

    Updates

---
+++ precache-manifest.2e46829cd34c69c2a946745241e35554.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad02fd3c26ed193e357c497d2c56e615",
+    "url": "/MovieDB/static/media/no-image.ad02fd3c.jpg"
+  },
+  {
+    "revision": "6fec01240c20f80e23c8",
+    "url": "/MovieDB/static/js/runtime~main.04edea3f.js"
+  },
+  {
+    "revision": "045d778608aa2233dc00",
+    "url": "/MovieDB/static/js/main.1aed918f.chunk.js"
+  },
+  {
+    "revision": "a150092d6a5166528044",
+    "url": "/MovieDB/static/js/2.6d79ba56.chunk.js"
+  },
+  {
+    "revision": "045d778608aa2233dc00",
+    "url": "/MovieDB/static/css/main.589baadc.chunk.css"
+  },
+  {
+    "revision": "3a12e77c253dad0d5fb3370e6d9f7104",
+    "url": "/MovieDB/index.html"
+  }
+];
